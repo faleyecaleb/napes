92d6c55d170a112a8fb6650070f90cc0702cb356
unknown <faleyecassiel005@gmail.com>
2022-10-09 21:14:25 -0700

    update

--- napes/components/ExecutivesCard.tsx
+++ napes/components/ExecutivesCard.tsx
@@ -8,7 +8,7 @@ interface Executives {
 }
 const ExecutivesCard: React.FC<Executives> = ({ image, title, name, desc }) => {
   return (
-    <div className='flex flex-col justify-center rounded shadow shadow-gray-600 items-center relative pl-3 pr-2 bg-white pb-10 to-blue-500'>
+    <div className='flex flex-col justify-center rounded shadow shadow-gray-600 items-center relative pl-3 pr-2  pb-10 to-blue-500'>
       <img className='w-1/2 h-52 md:h-52 mt-6 md:w-2/3 lg:h-52 rounded-full object-cover object-center md:-mt-10 top-0' src={image} alt={title} />
       <p className='text-white text-md mb-3'>{title}</p>
       <h2 className='font-bold text-gray-800 mb-3 text-xl'> {name} </h2>
